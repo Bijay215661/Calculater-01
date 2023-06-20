7606d827e69697e39a9918abe66cf3dfa4bbe3da
Bijay tamang <bunnytamang349@gmail.com>
2023-06-20 21:32:10 +0545

    Addition

---
+++ calculater.py
@@ -0,0 +1,12 @@
+ad =  "1"
+
+print(ad, "Addition")
+choise = input("Enter your chpoise : ")
+ 
+if choise == ad:
+    big_number = int(input("Enter the first number to add: "))
+    second_number = int(input("Enter the Second numbe: "))
+
+    total_add = big_number +  second_number
+    print("This is the added value: ", total_add)
+
