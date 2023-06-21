f44480edbe2946711f7d1c4cfdc29990a4f7dadf
Bijay tamang <bunnytamang349@gmail.com>
2023-06-21 11:01:14 +0545

    Adding while and again

--- calculater.py
+++ calculater.py
@@ -1,6 +1,7 @@
-ad =  "1"
+again =True
 
-sb = 2
+ad =  "1"
+sb = "2"
 
 print(ad, "Addition")
 print(sb, "Subraction")
@@ -21,5 +22,37 @@ elif choise == sb:
 
     sub_total = fst_bg_number - sc_number
     print("This is the subracted number: ", sub_total)
+
+continue_again = input("Do you want to Contunue y/n: ")
+if continue_again == "n":
+    again = False
+
+while again == True:
+    ad =  "1"
+    sb = "2"
+
+    print(ad, "Addition")
+    print(sb, "Subraction")
+    choise = input("Enter your chpoise : ")
+
+
+    
+    if choise == ad:
+        big_number = int(input("Enter the first number to add: "))
+        second_number = int(input("Enter the Second numbe: "))
+
+        total_add = big_number +  second_number
+        print("This is the added value: ", total_add)
+
+    elif choise == sb:
+        fst_bg_number = int(input("Enter the first large number: "))
+        sc_number = int(input("Enter the second number: "))
+
+        sub_total = fst_bg_number - sc_number
+        print("This is the subracted number: ", sub_total)
+
+    continue_again = input("Do you want to Contunue y/n: ")
+    if continue_again == "n":
+        again = False
 
     
